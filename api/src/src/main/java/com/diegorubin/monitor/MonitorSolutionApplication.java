2c8724e1b08eb44a7c408bb75931b52f87d42b8d
Diego Rubin <rubin.diego@gmail.com>
2016-07-11 16:48:42 -0300

    creating repo.

---
+++ api/src/src/main/java/com/diegorubin/monitor/MonitorSolutionApplication.java
@@ -0,0 +1,12 @@
+package com.diegorubin.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorSolutionApplication.class, args);
+	}
+}
