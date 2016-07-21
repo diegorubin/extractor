990807900243ca3a108dba71d0b2249a4ae9c140
Diego Rubin <diegorubin@ciandt.com>
2016-07-21 09:30:54 -0300

    working with service discovery server.

---
+++ eureka/src/main/java/com/diegorubin/tarzan/eureka/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.diegorubin.tarzan.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Server Application
+ *
+ * @author diegorubin
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServerApplication.class, args);
+  }
+
+}
--- worker/src/main/java/com/diegorubin/tarzan/worker/WorkerApplication.java
+++ worker/src/main/java/com/diegorubin/tarzan/worker/WorkerApplication.java
@@ -14,7 +14,6 @@ public class WorkerApplication implements CommandLineRunner {
   @Autowired
   private TwitterGateway twitterGateway;
 
-
   public static void main(String[] args) {
     SpringApplication.run(WorkerApplication.class, args);
   }
